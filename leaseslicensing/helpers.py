908ee60de6524784f958d14d6afb8767d136734a
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-12-15 14:48:26 +0800

    Update helpers

--- leaseslicensing/helpers.py
+++ leaseslicensing/helpers.py
@@ -24,38 +24,41 @@ def belongs_to(user, group_name):
 
     #return user.groups.filter(name=group_name).exists()
 
-def is_model_backend(request):
-    # Return True if user logged in via single sign-on (i.e. an internal)
-    return 'ModelBackend' in request.session.get('_auth_user_backend')
-
-def is_email_auth_backend(request):
-    # Return True if user logged in via social_auth (i.e. an external user signing in with a login-token)
-    return 'EmailAuth' in request.session.get('_auth_user_backend')
+#def is_model_backend(request):
+#    # Return True if user logged in via single sign-on (i.e. an internal)
+#    return 'ModelBackend' in request.session.get('_auth_user_backend')
+#
+#def is_email_auth_backend(request):
+#    # Return True if user logged in via social_auth (i.e. an external user signing in with a login-token)
+#    return 'EmailAuth' in request.session.get('_auth_user_backend')
 
 def is_leaseslicensing_admin(request):
     #logger.info('settings.ADMIN_GROUP: {}'.format(settings.ADMIN_GROUP))
-    return request.user.is_authenticated and is_model_backend(request) and in_dbca_domain(request) and (belongs_to(request.user, settings.ADMIN_GROUP))
+    return request.user.is_authenticated and (belongs_to(request.user, settings.ADMIN_GROUP) or request.user.is_superuser)
 
 def in_dbca_domain(request):
-    #import ipdb; ipdb.set_trace()
-    user = request.user
-    domain = user.email.split('@')[1]
-    if domain in settings.DEPT_DOMAINS:
-        if not user.is_staff:
-            # hack to reset department user to is_staff==True, if the user logged in externally (external departmentUser login defaults to is_staff=False)
-            user.is_staff = True
-            user.save()
-        return True
-    return False
+    return request.user.is_staff
+#    #import ipdb; ipdb.set_trace()
+#    user = request.user
+#    domain = user.email.split('@')[1]
+#    if domain in settings.DEPT_DOMAINS:
+#        if not user.is_staff:
+#            # hack to reset department user to is_staff==True, if the user logged in externally (external departmentUser login defaults to is_staff=False)
+#            user.is_staff = True
+#            user.save()
+#        return True
+#    return False
 
 def is_in_organisation_contacts(request, organisation):
     return request.user.email in organisation.contacts.all().values_list('email', flat=True)
 
 def is_departmentUser(request):
-    return request.user.is_authenticated and is_model_backend(request) and in_dbca_domain(request)
+    #return request.user.is_authenticated and is_model_backend(request) and in_dbca_domain(request)
+    return request.user.is_authenticated and request.user.is_staff
 
 def is_customer(request):
-    return request.user.is_authenticated and is_email_auth_backend(request)
+    #return request.user.is_authenticated and is_email_auth_backend(request)
+    return request.user.is_authenticated and not request.user.is_staff
 
 def is_internal(request):
     return is_departmentUser(request)
