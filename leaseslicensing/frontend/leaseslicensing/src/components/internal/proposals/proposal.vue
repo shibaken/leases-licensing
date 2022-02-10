4a1ded99310d29bccd1085030ef58232c29eabdb
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-02-10 15:19:38 +0800

    Create a component for handling a QA

--- leaseslicensing/frontend/leaseslicensing/src/components/internal/proposals/proposal.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/internal/proposals/proposal.vue
@@ -76,58 +76,15 @@
                                     <div class="assessment_title">Assessor</div>
                                 </template>
                                 <template v-for="question in assessment_for_assessor_map">  <!-- There is only one assessor assessment -->
-                                    <template v-if="question.accessing_user_can_view">
-                                        <div class="row form-group">
-                                            <div class="col-md-3">
-                                                <label for="">{{ question.checklist_question.text }}</label>
-                                            </div>
-                                            <div class="col-md-9">
-                                                <template v-if="question.checklist_question.answer_type=='free_text'">
-                                                    <textarea class="form-control free_text_area" :id="'free_text_' + question.id" v-model="question.answer_text" :disabled="!question.accessing_user_can_answer"/>
-                                                </template>
-                                                <template v-else>
-                                                    <div>
-                                                        <input type="radio" :id="'answer_yes_' + question.id" value="true" v-model="question.answer_yes_no" :disabled="!question.accessing_user_can_answer">
-                                                        <label :for="'answer_yes_' + question.id">Yes</label>
-                                                    </div>
-                                                    <div>
-                                                        <input type="radio" :id="'answer_no_' + question.id" value="false" v-model="question.answer_yes_no" :disabled="!question.accessing_user_can_answer">
-                                                        <label :for="'answer_no_' + question.id">No</label>
-                                                    </div>
-                                                </template>
-                                            </div>
-                                        </div>
-                                    </template>
+                                    <ChecklistQuestion :question="question" />
                                 </template>
 
                                 <template v-for="assessment in assessments_for_referrals_map"> <!-- There can be multiple referral assessments -->
                                     <div class="assessment_title">Referral: {{ assessment.referral_fullname }}</div>
                                     <template v-for="question in assessment.answers"> <!-- per question -->
-                                        <template v-if="question.accessing_user_can_view">
-                                            <div class="row form-group">
-                                                <div class="col-md-3">
-                                                    <label for="">{{ question.checklist_question.text }}</label>
-                                                </div>
-                                                <div class="col-md-9">
-                                                    <template v-if="question.checklist_question.answer_type=='free_text'">
-                                                        <textarea class="form-control free_text_area" :id="'free_text_' + question.id" v-model="question.answer_text" :disabled="!question.accessing_user_can_answer"/>
-                                                    </template>
-                                                    <template v-else>
-                                                        <div>
-                                                            <input type="radio" :id="'answer_yes_' + question.id" value="true" v-model="question.answer_yes_no" :disabled="!question.accessing_user_can_answer">
-                                                            <label :for="'answer_yes_' + question.id">Yes</label>
-                                                        </div>
-                                                        <div>
-                                                            <input type="radio" :id="'answer_no_' + question.id" value="false" v-model="question.answer_yes_no" :disabled="!question.accessing_user_can_answer">
-                                                            <label :for="'answer_no_' + question.id">No</label>
-                                                        </div>
-                                                    </template>
-                                                </div>
-                                            </div>
-                                        </template>
+                                        <ChecklistQuestion :question="question" />
                                     </template>
                                 </template>
-
                             </CollapsibleQuestions>
                         </template>
 
@@ -220,6 +177,7 @@ import { api_endpoints, helpers, constants } from '@/utils/hooks'
 import ApplicationForm from '@/components/form.vue';
 import FormSection from "@/components/forms/section_toggle.vue"
 import CollapsibleQuestions from '@/components/forms/collapsible_component.vue'
+import ChecklistQuestion from '@/components/common/component_checklist_question.vue'
 
 export default {
     name: 'InternalProposal',
@@ -314,6 +272,7 @@ export default {
         ApplicationForm,
         FormSection,
         CollapsibleQuestions,
+        ChecklistQuestion,
     },
     props: {
         proposalId: {
