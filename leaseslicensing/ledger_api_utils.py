58762e2134d196a03328b0fe4606a838dfe0db5e
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-02-24 16:47:34 +0800

    Add error logging to retrieve_email_user

--- leaseslicensing/ledger_api_utils.py
+++ leaseslicensing/ledger_api_utils.py
@@ -6,5 +6,19 @@ logger = logging.getLogger(__name__)
 
 
 def retrieve_email_user(email_user_id):
-    return EmailUser.objects.get(id=email_user_id)
+        try:
+            return EmailUser.objects.get(id=email_user_id)
+        except serializers.ValidationError:
+            print(traceback.print_exc())
+            raise serializers.ValidationError(str(e))
+        except ValidationError as e:
+            if hasattr(e,'error_dict'):
+                raise serializers.ValidationError(repr(e.error_dict))
+            else:
+                if hasattr(e,'message'):
+                    raise serializers.ValidationError(e.message)
+        except Exception as e:
+            print(traceback.print_exc())
+            raise serializers.ValidationError(str(e))
+
 
