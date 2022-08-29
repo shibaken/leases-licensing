a10aaf3fcdd8b319e73958ef5698a41576ac420e
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-08-29 16:26:45 +0800

    Working on expander...not completed

--- leaseslicensing/frontend/leaseslicensing/src/components/common/table_parties.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/table_parties.vue
@@ -13,7 +13,6 @@
                     :id="datatable_id"
                     :dtHeaders="datatable_headers"
                     :dtOptions="datatable_options"
-                    :key="datatable_key"
                 />
             </div>
         </div>
@@ -36,12 +35,22 @@ export default {
         return {
             datatable_id: uuid(),
             datatable_key: uuid(),
+
+            // For expander
+            td_expand_class_name: 'expand-icon',
+            td_collapse_class_name: 'collapse-icon',
         }
     },
     components: {
         datatable,
     },
     created: function(){
+    },
+    mounted: function(){
+        let vm = this
+        vm.$nextTick(() => {
+            vm.addEventListeners();
+        });
     },
     computed: {
         column_id: () => {
@@ -144,9 +153,31 @@ export default {
                 language: {
                     processing: "<i class='fa fa-4x fa-spinner fa-spin'></i>"
                 },
+                createdRow: function(row, data, index){
+                    let $row = this.api().row(row)
+
+                    // $(row).child($('<div>aho</div>')).show()
+                    // console.log({row})
+                    // let row_jq = $(row)
+                    // console.log({row_jq})
+                    // let $row = vm.$refs.competitive_process_datatable.vmDataTable.row(row_jq)
+
+                    console.log({$row})
+                    // $row.child('aho').show()
+                },
                 rowCallback: function (row, competitive_process){
-                    let row_jq = $(row)
-                    row_jq.children().first().addClass(vm.td_expand_class_name)
+                    // console.log({row})
+                    // let row_jq = $(row)
+                    // row_jq.children().first().addClass(vm.td_expand_class_name)
+                    //row_jq.child('aho')
+                    // console.log(vm.$refs.competitive_process_datatable.vmDataTable)
+
+                    // let $row = vm.$refs.competitive_process_datatable.vmDataTable.row($row)
+                    // if($row.child.isShown()){
+                    //     console.log('shown')
+                    // } else {
+                    //     console.log('not shown')
+                    // }
                 },
                 responsive: true,
                 serverSide: false,
@@ -169,9 +200,82 @@ export default {
         }
     },
     methods: {
-        add_party_clicked: () => {
+        add_party_clicked: function (){
 
+        },
+        addEventListeners: function (){
+            let vm = this
+
+            vm.$refs.competitive_process_datatable.vmDataTable.on('click', 'td', function(e) {
+                console.log('clicked')
+
+                let td_link = $(this)
+                let tr = td_link.closest('tr')
+                let first_td = tr.children().first()
+
+                // Get full data of this row
+                let $row = vm.$refs.competitive_process_datatable.vmDataTable.row(tr)
+                let full_data = $row.data()
+
+                if($row.child.isShown()){
+                    $row.child.hide();
+                    first_td.removeClass(vm.td_collapse_class_name).addClass(vm.td_expand_class_name)
+                } else {
+                    first_td.removeClass(vm.td_expand_class_name).addClass(vm.td_collapse_class_name)
+                    $row.child('id: ' + full_data.id).show();
+                }
+            })
         },
     }
 }
 </script>
+
+<style>
+.collapse-icon {
+    cursor: pointer;
+}
+.collapse-icon::before {
+    top: 5px;
+    left: 4px;
+    height: 14px;
+    width: 14px;
+    border-radius: 14px;
+    line-height: 14px;
+    border: 2px solid white;
+    line-height: 14px;
+    content: '-';
+    color: white;
+    background-color: #d33333;
+    display: inline-block;
+    box-shadow: 0px 0px 3px #444;
+    box-sizing: content-box;
+    text-align: center;
+    text-indent: 0 !important;
+    font-family: 'Courier New', Courier monospace;
+    margin: 5px;
+}
+.expand-icon {
+    cursor: pointer;
+}
+.expand-icon::before {
+    top: 5px;
+    left: 4px;
+    height: 14px;
+    width: 14px;
+    border-radius: 14px;
+    line-height: 14px;
+    border: 2px solid white;
+    line-height: 14px;
+    content: '+';
+    color: white;
+    background-color: #337ab7;
+    display: inline-block;
+    box-shadow: 0px 0px 3px #444;
+    box-sizing: content-box;
+    text-align: center;
+    text-indent: 0 !important;
+    font-family: 'Courier New', Courier monospace;
+    margin: 5px;
+}
+
+</style>
