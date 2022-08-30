94fcd26899fb2860fd5901ed60d8ecaab87f3692
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-08-30 13:09:40 +0800

    Make custom child row work with automatic child row

--- leaseslicensing/frontend/leaseslicensing/src/components/common/table_parties.vue
+++ leaseslicensing/frontend/leaseslicensing/src/components/common/table_parties.vue
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <datatable
-                    ref="competitive_process_datatable"
+                    ref="parties_datatable"
                     :id="datatable_id"
                     :dtHeaders="datatable_headers"
                     :dtOptions="datatable_options"
@@ -39,6 +39,7 @@ export default {
             // For expander
             td_expand_class_name: 'expand-icon',
             td_collapse_class_name: 'collapse-icon',
+            expandable_row_class_name: 'expandable_row_class_name',
         }
     },
     components: {
@@ -153,43 +154,28 @@ export default {
                 language: {
                     processing: "<i class='fa fa-4x fa-spinner fa-spin'></i>"
                 },
-                createdRow: function(row, data, index){
-                    let $row = this.api().row(row)
-
-                    // $(row).child($('<div>aho</div>')).show()
-                    // console.log({row})
-                    // let row_jq = $(row)
-                    // console.log({row_jq})
-                    // let $row = vm.$refs.competitive_process_datatable.vmDataTable.row(row_jq)
-
-                    console.log({$row})
-                    // $row.child('aho').show()
+                columnDefs: [
+                    {responsivePriority: 1, targets: 1},
+                    {responsivePriority: 2, targets: 2},
+                    {responsivePriority: 6, targets: 3},
+                    {responsivePriority: 5, targets: 4},
+                    {responsivePriority: 4, targets: 5},
+                    {responsivePriority: 3, targets: 6},
+                ],
+                createdRow: function(row, data, dataIndex){
+                    data.expanded = false
+                    console.log({data})
                 },
-                rowCallback: function (row, competitive_process){
-                    // console.log({row})
-                    // let row_jq = $(row)
-                    // row_jq.children().first().addClass(vm.td_expand_class_name)
-                    //row_jq.child('aho')
-                    // console.log(vm.$refs.competitive_process_datatable.vmDataTable)
-
-                    // let $row = vm.$refs.competitive_process_datatable.vmDataTable.row($row)
-                    // if($row.child.isShown()){
-                    //     console.log('shown')
-                    // } else {
-                    //     console.log('not shown')
-                    // }
+                rowCallback: function (row, aho){
+                    let $row = $(row)
+                    $row.children().first().addClass(vm.td_expand_class_name)
                 },
                 responsive: true,
                 serverSide: false,
                 data: vm.competitive_process_parties,
                 searching: search,
-                // ajax: {
-                //     "url": api_endpoints.competitive_process + '?format=datatables',
-                //     "dataSrc": 'data',
-                // },
                 dom: "<'d-flex align-items-center'<'me-auto'l>f>" +
                      "<'row'<'col-sm-12'tr>>" +
-                    //  "<'d-flex align-items-center'<'me-auto'i>p>",
                      "<'d-flex align-items-center'<'me-auto'i>>",
                 columns: columns,
                 processing: true,
@@ -200,31 +186,83 @@ export default {
         }
     },
     methods: {
+        number_of_columns: function() {
+            let vm = this
+            let num =  this.$refs.parties_datatable.vmDataTable.columns(':visible').nodes().length;
+            return num
+        },
+        updateColSpan: function(){
+            let vm = this
+            $('tr.' + vm.expandable_row_class_name + ' td').attr('colspan', vm.number_of_columns())
+        },
         add_party_clicked: function (){
 
         },
-        addEventListeners: function (){
+        addClickEventHandler: function(){
             let vm = this
-
-            vm.$refs.competitive_process_datatable.vmDataTable.on('click', 'td', function(e) {
-                console.log('clicked')
-
+            vm.$refs.parties_datatable.vmDataTable.on('click', 'td', function(e) {
                 let td_link = $(this)
+                if (!(td_link.hasClass(vm.td_expand_class_name) || td_link.hasClass(vm.td_collapse_class_name))){
+                    // This row is not configured as expandable row (at the rowCallback)
+                    return
+                }
                 let tr = td_link.closest('tr')
                 let first_td = tr.children().first()
 
                 // Get full data of this row
-                let $row = vm.$refs.competitive_process_datatable.vmDataTable.row(tr)
+                let $row = vm.$refs.parties_datatable.vmDataTable.row(tr)
+                console.log({$row})
                 let full_data = $row.data()
+                console.log({full_data})
 
-                if($row.child.isShown()){
-                    $row.child.hide();
+                if(full_data.expanded){
+                    // Collapse
+                    let siblings = tr.siblings('tr.' + vm.expandable_row_class_name)
+                    siblings.fadeOut(500, function(){
+                        siblings.remove()
+                    })
+
+                    // Change icon
                     first_td.removeClass(vm.td_collapse_class_name).addClass(vm.td_expand_class_name)
+                    // Hide child row, where hidden columns are
+                    $row.child.hide()
+                    // Update flag
+                    full_data.expanded = false
                 } else {
+                    // Expand
+                    let contents = 'Details here'
+                    let details_elem = $('<tr class="' + vm.expandable_row_class_name +'"><td>' + contents + '</td></tr>')
+                    details_elem.hide()
+                    details_elem.insertAfter(tr)
+                    vm.updateColSpan()
+                    details_elem.fadeIn(1000)
+
+                    // Change icon
                     first_td.removeClass(vm.td_expand_class_name).addClass(vm.td_collapse_class_name)
-                    $row.child('id: ' + full_data.id).show();
+                    // Show child row, where hidden columns are
+                    $row.child.show()
+                    // Update flag
+                    full_data.expanded = true
                 }
             })
+        },
+        addResponsiveResizeHandler: function(){
+            let vm = this
+            vm.$refs.parties_datatable.vmDataTable.on('responsive-resize', function(e, datatable, columns) {
+                vm.updateColSpan()
+                datatable.rows().every(function(rowIdx, tableLoop, rowLoop){
+                    let data = this.data()
+                    if (data.expanded){
+                        this.child.show()
+                    } else {
+                        this.child.hide()
+                    }
+                })
+            })
+        },
+        addEventListeners: function (){
+            this.addClickEventHandler()
+            this.addResponsiveResizeHandler()
         },
     }
 }
