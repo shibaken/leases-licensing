72d18d9ab0d40a102d21422b4e709e4aae75ed80
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-09-02 11:43:27 +0800

    Add models to handle temporary files

--- leaseslicensing/components/main/models.py
+++ leaseslicensing/components/main/models.py
@@ -415,6 +415,24 @@ class UserSystemSettings(models.Model):
         verbose_name_plural = "User System Settings"
 
 
+class TemporaryDocumentCollection(models.Model):
+
+    class Meta:
+        app_label = 'leaseslicensing'
+
+
+class TemporaryDocument(Document):
+    temp_document_collection = models.ForeignKey(
+        TemporaryDocumentCollection,
+        related_name='documents',
+        on_delete=models.CASCADE,
+    )
+    _file = models.FileField(max_length=255)
+
+    class Meta:
+        app_label = 'leaseslicensing'
+
+
 # import reversion
 # reversion.register(Region, follow=['districts'])
 # reversion.register(District, follow=['parks'])
