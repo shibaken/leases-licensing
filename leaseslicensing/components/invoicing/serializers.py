1c8b6d6dedfb5e229576bec60cb19276ec4a8873
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2022-10-11 16:34:34 +0800

    Working on validation

--- leaseslicensing/components/invoicing/serializers.py
+++ leaseslicensing/components/invoicing/serializers.py
@@ -145,10 +145,19 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
             'crown_land_rent_review_dates',     # ReverseFK
         )
 
+    def set_default_values(self, attrs, fields_excluded):
+        for attr_name, value in attrs.items():
+            if attr_name in ['base_fee_amount', 'once_off_charge_amount', 'review_once_every', 'review_repetition_type', 'invoicing_once_every', 'invoicing_repetition_type',]:
+                if attr_name not in fields_excluded:
+                    attrs[attr_name] = None  # Set default value
+            elif attr_name in ['annual_increment_amounts', 'annual_increment_percentages', 'gross_turnover_percentages', 'crown_land_rent_review_dates',]:
+                if attr_name not in fields_excluded:
+                    for item in self.initial_data.get(attr_name):
+                        item['to_be_deleted'] = True  # Mark as "to_be_deleted" to the initial value so that item is deleted at the update()
+
     def validate(self, attrs):
         action = self.context.get('action')
 
-        # TODO: conditional based on viewset.action?
         if action == 'finance_save':
             # When "Save and Continue"/"Save and Exit" button clicked
             pass
@@ -157,29 +166,85 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
             charge_method = attrs.get('charge_method')
 
             if charge_method.key == settings.CHARGE_METHOD_ONCE_OFF_CHARGE:
-                # attrs['charge_method'] = ''
-                attrs['base_fee_amount'] = None
-                # attrs['once_off_charge_amount'] = None
-                attrs['review_once_every'] = None
-                attrs['review_repetition_type'] = None
-                attrs['invoicing_once_every'] = None
-                attrs['invoicing_repetition_type'] = None
-                attrs['annual_increment_amounts'] = None
-                attrs['annual_increment_percentages'] = None
-                attrs['gross_turnover_percentages'] = None
-                attrs['crown_land_rent_review_dates'] = None
+                self.set_default_values(attrs, [
+                    'charge_method',
+                    'once_off_charge_amount',
+                ])
             elif charge_method.key == settings.CHARGE_METHOD_BASE_FEE_PLUS_FIXED_ANNUAL_INCREMENT:
-                pass
+                self.set_default_values(attrs, [
+                    'charge_method',
+                    'base_fee_amount',
+                    'annual_increment_amounts',
+                    'review_once_every',
+                    'review_repetition_type',
+                    'crown_land_rent_review_dates',
+                    'invoicing_once_every',
+                    'invoicing_repetition_type',
+                ])
+
+                annual_increment_amounts_data = attrs.get('annual_increment_amounts')
+                self.validate_annual_increment(annual_increment_amounts_data)
+                self.validate_crown_land_rent_review_dates(attrs)
+
             elif charge_method.key == settings.CHARGE_METHOD_BASE_FEE_PLUS_FIXED_ANNUAL_PERCENTAGE:
-                pass
+                self.set_default_values(attrs, [
+                    'charge_method',
+                    'base_fee_amount',
+                    'annual_increment_percentages',
+                    'review_once_every',
+                    'review_repetition_type',
+                    'crown_land_rent_review_dates',
+                    'invoicing_once_every',
+                    'invoicing_repetition_type',
+                ])
+                annual_increment_percentages_data = attrs.get('annual_increment_percentages')
+                self.validate_annual_increment(annual_increment_percentages_data)
+                self.validate_crown_land_rent_review_dates(attrs)
             elif charge_method.key == settings.CHARGE_METHOD_BASE_FEE_PLUS_ANNUAL_CPI:
-                pass
+                self.set_default_values(attrs, [
+                    'charge_method',
+                    'base_fee_amount',
+                    'review_once_every',
+                    'review_repetition_type',
+                    'crown_land_rent_review_dates',
+                    'invoicing_once_every',
+                    'invoicing_repetition_type',
+                ])
+                self.validate_crown_land_rent_review_dates(attrs)
             elif charge_method.key == settings.CHARGE_METHOD_PERCENTAGE_OF_GROSS_TURNOVER:
-                pass
+                self.set_default_values(attrs, [
+                    'charge_method',
+                    'gross_turnover_percentages',
+                    'invoicing_once_every',
+                    'invoicing_repetition_type',
+                ])
+                gross_turnover_percentages_data = attrs.get('gross_turnover_percentages')
+                self.validate_annual_increment(gross_turnover_percentages_data)
             elif charge_method.key == settings.CHARGE_METHOD_NO_RENT_OR_LICENCE_CHARGE:
-                pass
+                self.set_default_values(attrs, [])
 
         return attrs
+
+    def validate_annual_increment(self, annual_increment_data):
+        # Make sure there are no duplication of 'year'
+        years = []
+        for data in annual_increment_data:
+            a_year = data.get('year')
+            if a_year in years:
+                raise Exception
+            else:
+                years.append(a_year)
+
+    def validate_crown_land_rent_review_dates(self, attrs):
+        # Make sure there are no duplication of 'date'
+        crown_land_rent_review_dates_data = attrs.get('crown_land_rent_review_dates')
+        dates = []
+        for crown_land_rent_review_date_data in crown_land_rent_review_dates_data:
+            date = crown_land_rent_review_date_data.get('review_date')
+            if date in dates:
+                raise Exception('Review data duplicated')
+            else:
+                dates.append(date)
 
     def update(self, instance, validated_data):
         # Local fields
@@ -206,28 +271,32 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
         self.update_gross_turnover_percentages(gross_turnover_percentages_data, instance)
         self.update_crown_land_rent_review_dates(crown_land_rent_review_dates_data, instance)
 
-        # TODO: Do we allow to delete the existing items if it's allowed...?
-
         return instance
+
+    @staticmethod
+    def _to_be_deleted(a_data, initial_data):
+        to_be_deleted = False
+        for initial_data_row in initial_data:
+            if initial_data_row.get('id') == a_data.get('id'):
+                if initial_data_row.get('to_be_deleted'):
+                    to_be_deleted = True
+                    break
+        return to_be_deleted
 
     def update_annual_increment_amounts(self, validated_annual_increment_amounts_data, instance):
         initial_data = self.initial_data.get('annual_increment_amounts')
 
         for annual_increment_amount_data in validated_annual_increment_amounts_data:
             if annual_increment_amount_data.get('id', 0):
-                # Existing
+                # This data exists in the database
 
                 # Check if it is marked as to_be_deleted
-                to_be_deleted = False
-                for initial_data_row in initial_data:
-                    if initial_data_row.get('id') == annual_increment_amount_data.get('id'):
-                        if initial_data_row.get('to_be_deleted'):
-                            to_be_deleted = True
+                to_be_deleted = self._to_be_deleted(annual_increment_amount_data, initial_data)
 
                 annual_increment_amount = FixedAnnualIncrementAmount.objects.get(id=int(annual_increment_amount_data.get('id')))
                 if to_be_deleted:
                     # Data is marked as to_be_deleted
-                    if not annual_increment_amount.readonly:
+                    if not annual_increment_amount.readonly:  # Double check if it's not readonly data.
                         annual_increment_amount.delete()
                 else:
                     # Update data
@@ -237,7 +306,7 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
                     serializer.is_valid(raise_exception=True)
                     serializer.save()
             else:
-                # New
+                # This is new data, not stored in the database yet.
                 if 'id' in annual_increment_amount_data:
                     annual_increment_amount_data.pop('id')  # Delete the item 'id: 0' from the dictionary because we don't want to save a new record with id=0
                 serializer = FixedAnnualIncrementAmountSerializer(data=annual_increment_amount_data, context={'invoicing_details': instance})
@@ -251,14 +320,10 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
 
         for annual_increment_percentage_data in validated_annual_increment_percentages_data:
             if annual_increment_percentage_data.get('id', 0):
-                # Existing
+                # This data exists in the database
 
                 # Check if it is marked as to_be_deleted
-                to_be_deleted = False
-                for initial_data_row in initial_data:
-                    if initial_data_row.get('id') == annual_increment_percentage_data.get('id'):
-                        if initial_data_row.get('to_be_deleted'):
-                            to_be_deleted = True
+                to_be_deleted = self._to_be_deleted(annual_increment_percentage_data, initial_data)
 
                 annual_increment_percentage = FixedAnnualIncrementPercentage.objects.get(id=int(annual_increment_percentage_data.get('id')))
                 if to_be_deleted:
@@ -273,7 +338,7 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
                     serializer.is_valid(raise_exception=True)
                     serializer.save()
             else:
-                # New
+                # This is new data, not stored in the database yet.
                 if 'id' in annual_increment_percentage_data:
                     annual_increment_percentage_data.pop('id')  # Delete the item 'id: 0' from the dictionary because we don't want to save a new record with id=0
                 serializer = FixedAnnualIncrementPercentageSerializer(data=annual_increment_percentage_data, context={'invoicing_details': instance})
@@ -287,14 +352,10 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
 
         for gross_turnover_percentage_data in validated_gross_turnover_percentages_data:
             if gross_turnover_percentage_data.get('id', 0):
-                # Existing
+                # This data exists in the database
 
                 # Check if it is marked as to_be_deleted
-                to_be_deleted = False
-                for initial_data_row in initial_data:
-                    if initial_data_row.get('id') == gross_turnover_percentage_data.get('id'):
-                        if initial_data_row.get('to_be_deleted'):
-                            to_be_deleted = True
+                to_be_deleted = self._to_be_deleted(gross_turnover_percentage_data, initial_data)
 
                 gross_turnover_percentage = PercentageOfGrossTurnover.objects.get(id=int(gross_turnover_percentage_data.get('id')))
                 if to_be_deleted:
@@ -309,7 +370,7 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
                     serializer.is_valid(raise_exception=True)
                     serializer.save()
             else:
-                # New
+                # This is new data, not stored in the database yet.
                 if 'id' in gross_turnover_percentage_data:
                     gross_turnover_percentage_data.pop('id')  # Delete the item 'id: 0' from the dictionary because we don't want to save a new record with id=0
                 serializer = PercentageOfGrossTurnoverSerializer(data=gross_turnover_percentage_data, context={'invoicing_details': instance})
@@ -323,14 +384,10 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
 
         for crown_land_rent_review_date_data in validated_crown_land_rent_review_dates_data:
             if crown_land_rent_review_date_data.get('id', 0):
-                # Existing
+                # This data exists in the database
 
                 # Check if it is marked as to_be_deleted
-                to_be_deleted = False
-                for initial_data_row in initial_data:
-                    if initial_data_row.get('id') == crown_land_rent_review_date_data.get('id'):
-                        if initial_data_row.get('to_be_deleted'):
-                            to_be_deleted = True
+                to_be_deleted = self._to_be_deleted(crown_land_rent_review_date_data, initial_data)
 
                 crown_land_rent_review_date = CrownLandRentReviewDate.objects.get(id=int(crown_land_rent_review_date_data.get('id')))
                 if to_be_deleted:
@@ -343,7 +400,7 @@ class InvoicingDetailsSerializer(serializers.ModelSerializer):
                     serializer.is_valid(raise_exception=True)
                     serializer.save()
             else:
-                # New
+                # This is new data, not stored in the database yet.
                 if 'id' in crown_land_rent_review_date_data:
                     crown_land_rent_review_date_data.pop('id')  # Delete the item 'id: 0' from the dictionary because we don't want to save a new record with id=0
                 serializer = CrownLandRentReviewDateSerializer(data=crown_land_rent_review_date_data, context={'invoicing_details': instance})
